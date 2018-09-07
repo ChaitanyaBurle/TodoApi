df900f49d5701e99a6ecc8391bd85aaea961bcda
Chaitanya Pavan Kumar Burle <cburle@corp.miraclesoft.com>
2018-09-07 16:49:14 +0530

    Hashing Passwords

--- server/models/user.js
+++ server/models/user.js
@@ -3,6 +3,8 @@ const validator =require('validator');
 
 const jwt = require('jsonwebtoken');
 const _ =require('lodash');
+const bcrypt = require('bcryptjs');
+
 var UserSchema = new mongoose.Schema({
         email:{
             type:String,
@@ -70,8 +72,26 @@ UserSchema.statics.findByToken = function(token){
         'tokens.token':token,
         'tokens.access':'auth'
 
-    })
-}
+    });
+};
+
+UserSchema.pre('save',function(next){
+    var user =this;
+    if(user.isModified('password')){
+        bcrypt.genSalt(10,(err,salt)=>{
+            bcrypt.hash(user.password,salt,(err,hash)=>{
+                user.password=hash;
+                next();
+
+            });
+        });
+
+    }
+    else{
+        next();
+    }
+
+});
 
 var User = mongoose.model('Employee',UserSchema);
 
